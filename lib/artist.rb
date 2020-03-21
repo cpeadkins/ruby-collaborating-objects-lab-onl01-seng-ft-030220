5525fbfd994c56cf804ccdcb1686576b2baa304c
Charles Adkins <cpeadkins@gmail.com>
2020-03-21 20:55:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,4 +17,12 @@ class Artist
     @songs << song
   end
 
+  def self.find_or_create_by_name(name)
+    self.all.detect {| artist | artist.name == name} || Artist.new(name)
+  end
+
+  def print_songs
+    self.songs.each {| song | puts song.name}
+  end
+
 end
